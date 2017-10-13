491bc36a65713dac883babdc15fa86d3d6f4f9c0
Almeida Jessica <almeida.jessica@outlook.com>
2017-10-13 19:14:25 +0200

    Add PATCH /todos/.:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
@@ -60,6 +61,32 @@ app.delete('/todos/:id', (req, res) => {
         if (!todo) {
             return res.sendStatus(404).send();
         }
+        res.send({ todo });
+    }).catch((e) => {
+        res.sendStatus(400).send();
+    })
+})
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.sendStatus(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
         res.send({ todo });
     }).catch((e) => {
         res.sendStatus(400).send();
